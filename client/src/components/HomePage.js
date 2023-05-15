7ace0e0136b842a3036a3b9925c8912936949c5a
Jess Espinoza <jessica.espinoza2@gmail.com>
2023-05-15 16:25:55 -0400

    Initial app fix. added text to render on home page

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const HomePage = (props) => {
+    return(
+        <div className="home-title">
+            <h1>Birds Eye View</h1>
+            <h3>A Birding App for Bird Enthusiasts!</h3>
+        
+        <div>
+            <p>Developed by Jessica Espinoza</p>
+        </div>
+        </div>
+    )
+}
+
+export default HomePage
