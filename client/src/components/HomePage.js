20c376e134e15b2957081999e9cb5a4230448927
Jess Espinoza <jessica.espinoza2@gmail.com>
2023-05-16 13:09:34 -0400

    added image

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,9 +3,12 @@ import React from "react"
 const HomePage = (props) => {
     return(
         <div className="home-title">
-            <h1>Birds Eye View</h1>
+                <img className="home-image" src="https://bird-application.s3.amazonaws.com/Hotpot+(2).png"
+                    alt="bird app logo"> 
+                </img>
+            <h2 >Birds Eye View</h2>
             <h3>A Birding App for Bird Enthusiasts!</h3>
-        
+                
         <div>
             <p>Developed by Jessica Espinoza</p>
         </div>
