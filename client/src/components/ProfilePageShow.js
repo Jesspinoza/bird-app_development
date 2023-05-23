860d0c9882dddd6adf2ea0571b7f0b92c19abeda
Jess Espinoza <jessica.espinoza2@gmail.com>
2023-05-23 16:35:56 -0400

    merge in from main

---
+++ client/src/components/ProfilePageShow.js
@@ -0,0 +1,15 @@
+import React from "react"
+   
+const ProfilePageShow = (props) => {
+
+        return (
+         <>
+            <h3>Username:</h3>
+            <h3>Email:</h3>
+            <h3>Bird Sightings!</h3>
+            <h3>Dream Birds</h3>
+        </>
+    )
+            
+}
+export default ProfilePageShow
